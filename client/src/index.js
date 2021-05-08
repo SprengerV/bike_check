092655e2f86eeca0b9eff8ead02313175f0e2fc0
SprengerV <b.francis@sprengerv.com>
2021-05-08 13:33:01 -0400

    added auth0

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain=""
+      clientId="56KCkjC82g0QNP75lXVhyBiE97QXX7Li"
+      redirectUri="sprengerv.us.auth0.com"
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
