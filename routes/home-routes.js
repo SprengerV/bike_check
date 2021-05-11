7eb0c52adef300e8afa038798532104352d19e04
Hanna Munoz <hannamunoz@gmail.com>
2021-05-10 21:11:10 -0400

    Working on routes

---
+++ routes/home-routes.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const { MODELS HERE } = require('./PATHTO MODELS');
+const userAuth = require('PATHTOAUTH');
+// do we need a timestamp helper here for post times?
+
+// GET for homepage
+router.get('/', (req, res) => {
+
+});
+
+// GET user login
+router.get('/login', (req, res) => {
+    
+})
