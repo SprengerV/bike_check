d478269151bb55d39df8fa71e806e25c69152316
SprengerV <b.francis@sprengerv.com>
2021-05-11 03:17:15 -0400

    comment model

---
+++ models/Comment.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const User = require('./User');
+
+class Comment extends Model {};
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4
+        },
+        userId: {
+            type: DataTypes.TEXT,
+            allowNull: false
+            references: {
+                model: User,
+                key: 'id'
+            }
+        },
+        bikeId: {
+            type: DataTypes.UUID,
+            allowNull: false,
+            references: {
+                model: Bike,
+                key: 'id'
+            }
+        },
+        body: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        underscored: true,
+        modelName: 'comments'
+    }
+)
