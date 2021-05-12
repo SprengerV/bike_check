08bbe7552f5541229ab6c8d96ea825d287f32e6e
Hanna Munoz <hannamunoz@gmail.com>
2021-05-12 18:49:28 -0400

    uodated home-routes

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,11 +1,18 @@
 const router = require('express').Router();
-const { MODELS HERE } = require('./PATHTO MODELS');
+const { Bike, Comment, Like, Photo, User  } = require('../models');
 const userAuth = require('PATHTOAUTH');
 // do we need a timestamp helper here for post times?
 
 // GET for homepage
 router.get('/', (req, res) => {
-
+    Bike.findAll({
+        attributes: [
+            'id',
+            'userId',
+            'body',
+            'updated'
+        ]
+    })
 });
 
 // GET user login
