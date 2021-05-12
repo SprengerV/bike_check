4957e96c679d8cec4d33b79fe63d1b4fa8193256
SprengerV <b.francis@sprengerv.com>
2021-05-12 19:46:14 -0400

    added authRoutes to be used for creating db user

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ const router = require("express").Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const profileRoutes = require('./profile-routes');
+const authRoutes = require('./auth-routes')
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 router.use('/profile', profileRoutes);
+router.use('/auth', authRoutes);
 
 
 
