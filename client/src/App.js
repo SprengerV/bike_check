9818a97b0bc3278c39be1f71a840c1e16f700c72
taylorgonz <hello@taylorgonz.com>
2021-05-12 19:04:24 -0400

    image added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import AuthenticationButton from './Components/authenticationButton';
+import logo from "../images/bike-check-logo"
 
-function App() {
+function App() { 
   return (
     <div className="App">
+      <nav className="navBar navbar-expand-lg">
+        <img src={logo}/>
+      </nav>
       <AuthenticationButton/>
     </div>
   );
