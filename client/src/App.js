c92e3e3b7fcff9fbcc61d21337a46b86cd3a1a3f
taylorgonz <hello@taylorgonz.com>
2021-05-12 21:26:08 -0400

    navbar and login/logoutbutton set

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 import AuthenticationButton from './Components/authenticationButton';
-import logo from "../images/bike-check-logo"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import logo from "./images/bike-check-logo.png";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
-function App() { 
+
+function App() {
   return (
-    <div className="App">
-      <nav className="navBar navbar-expand-lg">
-        <img src={logo}/>
-      </nav>
-      <AuthenticationButton/>
-    </div>
+    <Router>
+      <Container className="p-0" fluid={true}>
+
+      <Navbar className='Navbar navbar-dark navigation ' bg="transparent" expand="lg">
+        <Navbar.Brand className='nav-fonts'><img href='/' src={logo} className="headerLogo"/></Navbar.Brand>
+        <Navbar.Toggle className='border-0' aria-controls="navbar-toggle" />
+        <Navbar.Collapse id="navbar-toggle">
+          <Nav className='linkNav'>
+            <Link className="homeNav" to='/'> Home</Link>
+            <Link className="homeNav" to='/Profile:id'> Profile</Link>
+            <AuthenticationButton/>
+          </Nav>
+          </Navbar.Collapse>
+      </Navbar>
+
+      </Container>
+    </Router>
   );
 }
 
