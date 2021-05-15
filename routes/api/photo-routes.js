ed2ebf6a84a4af339b0aba471fd5879a5999a677
Hanna Munoz <hannamunoz@gmail.com>
2021-05-15 12:58:44 -0400

    working on photo routes

---
+++ routes/api/photo-routes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Bike, Comment, Like, Photo, User  } = require('../../models');
+
+// Get all photos
+router.get('/', (req, res) => {
+    Photo.findAll({
+        attributes: [
+            'id',
+            'bikeId',
+            'userId',
+            'url',
+            'uploaded',
+        ],
+    })
+    .then(photoData => res.json(photoData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    });
+});
