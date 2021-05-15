aa7e04191602202bfbb3cf02c45f16c8507b9144
Tristan LaRoche <laroche11235@gmail.com>
2021-05-15 10:22:00 -0400

    made file for component post

---
+++ client/src/Components/Post/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Container, Accordion, Card, Button, DropdownButton, Dropdown, FormControl } from 'react-bootstrap';
+// import DropdownItem from 'react-bootstrap/esm/DropdownItem';
+
+const Post = () => {
+    return (
+        
+    )
+}
+
+export default Post
