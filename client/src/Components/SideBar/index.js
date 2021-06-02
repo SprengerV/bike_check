879a8a27aeda10a88c1c56d0ad3eb933a4b9d424
taylorgonz <hello@taylorgonz.com>
2021-06-02 16:43:55 -0400

    couple mobile styling issues

--- client/src/Components/SideBar/index.js
+++ client/src/Components/SideBar/index.js
@@ -16,7 +16,7 @@ const SideBar = (props) => {
       </Card.Header>
       <Card.Body className="row">
         {cats.map((cat, i) => <>
-        <div className="col-xs-6 col-md-12">
+        <div className="col-6 col-lg-12 col-md-12">
           <button onClick={() => props.getPosts({cat})} className="catBtn"   key={i} variant="light"><a className="sideBarRefs" href={`/category/${cat}`}> {cat}</a> </button>
           </div>
         </>)}
